ced45987f3315dfdac7d3602beefa86e2ca20e9a
Scott Vandehey <fake-scott@spaceninja.com>
2018-11-28 09:47:55 -0800

    13 our first query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    console.log(args);
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    // return [{ name: "Snickers" }, { name: "Sunny" }];
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
