5ce5630b08949278f4bcae997b22b16392ec615f
Scott Vandehey <fake-scott@spaceninja.com>
2018-11-30 18:05:57 -0800

    18 uploading images

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,25 @@ class CreateItem extends React.Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    console.log("uploading file…");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/spaceninja/image/upload", {
+      method: "POST",
+      body: data
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.eager[0].secure_url,
+      largeImage: file.eager[1].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -60,35 +79,53 @@ class CreateItem extends React.Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Title"
-                required
-                value={this.state.title}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="price">Price</label>
-              <input
-                type="number"
-                id="price"
-                name="price"
-                placeholder="Price"
-                required
-                value={this.state.price}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="description">Description</label>
-              <textarea
-                id="description"
-                name="description"
-                placeholder="Enter a Description"
-                required
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img width="200" src={this.state.image} alt="Image Preview" />}
+              </label>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter a Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
               <button type="submit">Submit</button>
             </fieldset>
           </Form>
